4f09eeec4809eae07e2c35c781e25bbd75b675a4
K0OOL <ataqah.ramadhan@gmail.com>
2026-03-03 19:41:20 +0700

    pembaruan m²

--- latihan/src/tugas1.java
+++ latihan/src/tugas1.java
@@ -10,7 +10,7 @@ public class tugas1 {
         System.out.print("Masukan Panjang Ruangan (meter) = ");
         double p = input.nextDouble();
         Double luas = (2 * (p + l)) * t;
-        System.out.printf("Luas Permukaan Tembok = %.2f Meter persegi", luas);
+        System.out.printf("Luas Permukaan Tembok = %.2f m²%n", luas);
         System.out.println("Cat yang dibutuhkan = " + Math.ceil(luas / 10) + " Liter");
         input.close();
     }
