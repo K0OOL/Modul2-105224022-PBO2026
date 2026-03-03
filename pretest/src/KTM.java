f1f82dd20c0c709111c4d5bbefd68495be88f858
K0OOL <ataqah.ramadhan@gmail.com>
2026-03-03 15:01:27 +0700

    first commit

---
+++ pretest/src/KTM.java
@@ -0,0 +1,7 @@
+public class KTM {
+    public static void main(String[] args) {
+        System.out.println("============================");
+        System.out.println("Nama\t: Ataqah Ramadhan WP\nNIM\t: 105224022\nProdi\t: Ilmu Komputer");
+        System.out.println("============================");
+    }
+}
